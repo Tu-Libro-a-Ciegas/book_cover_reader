32daf8376f83a37ab9f3d86edd884ad5ceb4b738
Alejandra_Berbesi <berbesi.alejandra@gmail.com>
2019-09-13 09:57:55 -0500

    borrador

--- src/main.py
+++ src/main.py
@@ -10,23 +10,36 @@ from storage_script import list_blobs, move_blob
 from vision_script import parse_vision_description
 from utils import format_q_search, write_ndjson_file, write_ndjson_error_file
 
-
 bkt_todo = os.environ.get('bkt_todo')
 bkt_done = os.environ.get('bkt_done')
 bkt_failed = os.environ.get('bkt_failed')
 book_table = os.environ.get('book_table')
 error_table = os.environ.get('error_table')
 
-for cover in list_blobs(bkt_todo):
-    try:
-        cover_text = parse_vision_description(cover)
-        cover_text = format_q_search(cover_text)
-        bjson = construct_json_query(cover_text)
-        write_ndjson_file(bjson)
-        load_book_from_ndjson(book_table)
-    except Exception as e:
-        write_ndjson_error_file(cover, str(e))
-        load_book_from_ndjson(error_table)
-        move_blob(bkt_todo, cover, bkt_failed, cover)
+import re 
+
+def process_front_cover():
+    if list_blobs(bkt_todo) != []:
+        for cover in list_blobs(bkt_todo):
+            if re.search('^.*[.]1[.].*',cover):
+                try:
+                    cover_text = parse_vision_description(cover)
+                    cover_text = format_q_search(cover_text)
+                    bjson = construct_json_query(cover_text)
+                    print(cover)
+                    #write_ndjson_file(bjson)
+                    #load_book_from_ndjson(book_table)
+                except Exception as e:
+                    pass
+                    #write_ndjson_error_file(cover, str(e))
+                    #load_book_from_ndjson(error_table)
+                    #move_blob(bkt_todo, cover, bkt_failed, cover)
+                else:
+                    pass
+                    #move_blob(bkt_todo, cover, bkt_done, cover)
     else:
-        move_blob(bkt_todo, cover, bkt_done, cover)
+        print("no results")
+
+process_front_cover()
+
+
